21dfba9a5b2d50532efc245d0ece282d1bed1e22
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-06-26 00:07:11 -0600

    add configuracion de conexion en db

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
 <?php
 function conexion() {
-    return new PDO('mysql:host=localhost;dbname=KalliPos', 'root', 'root');
+    return new PDO('mysql:host=localhost:3306;dbname=kallijag_pos_stage', 'kallijag_stage', 'uNtiL.horSe@5');
 }
