3b6eee4330624c72857d293162cd4aad68b279eb
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-06-26 18:10:34 -0600

    add new style to header

--- views/header.php
+++ views/header.php
@@ -4,8 +4,9 @@
   <meta charset="UTF-8">
   <title>POS Kalli Jaguar</title>
   <link rel="icon" href="../assets/Logo.jpg" type="image/jpg">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
 <?php include 'navbar.php'; ?>
-<div class="container mt-4">
