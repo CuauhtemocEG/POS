17d6964b4067b13c54554e3fc977a94f6f0cb1e6
[CuauhtemocEG] <[temoc612@gmail.com]>
2025-07-16 14:41:14 -0600

    add formta

--- views/header.php
+++ views/header.php
@@ -8,8 +8,19 @@
   <link rel="icon" href="../assets/Logo.jpg" type="image/jpg">
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <style>
+    @media (min-width: 1600px) {
+      body {
+        font-size: 1.1rem;
+      }
+
+      .max-w-4xl {
+        max-width: 70rem !important;
+      }
+    }
+  </style>
 </head>
 
 <body class="bg-gradient-to-br from-blue-100 via-yellow-50 to-blue-50 min-h-screen">
   <?php include 'navbar.php'; ?>
-  <div class="max-w-4xl mx-auto mt-8 mb-6 bg-white rounded-2xl shadow-lg px-4 py-6 sm:px-8 sm:py-8">
+  <div class="max-w-4xl mx-auto mt-8 mb-6 bg-white rounded-2xl shadow-lg px-4 py-6 sm:px-8 sm:py-8 md:px-12 md:py-10 lg:px-16 lg:py-12 xl:px-24 xl:py-14">
